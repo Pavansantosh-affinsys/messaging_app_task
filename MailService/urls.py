7b6692db8c3710cb22ad4cc589021a9f50c06178
Pavan Santosh <pavan.santosh@affinsys.com>
2022-10-28 23:10:38 +0530

    accounts urls made

---
+++ MailService/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from MailService import views
+# from rest_framework import routers
+
+
+# router = routers.DefaultRouter()
+# router.register(r"AccountDetails", views.AccountMixin)
+
+
+urlpatterns = [
+    path("account/", views.AccountMixin.as_view(), name="accounts_gppd")
+]
+
