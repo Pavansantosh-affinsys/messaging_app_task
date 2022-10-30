a1561578092466274f1f5d960c2ee95822297a38
Pavan Santosh <pavan.santosh@affinsys.com>
2022-10-30 18:14:36 +0530

    minor changes

--- MailService/tasks.py
+++ MailService/tasks.py
@@ -2,8 +2,6 @@ from django.core.mail import send_mail
 from message_queue import settings
 
 from message_queue.celery import app
-from time import sleep
-from celery import shared_task
 
 
 @app.task(bind=True)
@@ -11,5 +9,4 @@ def email_recommendations(self, emails, description):
     subject = "Recommendations"
     message = f"{description}"
     email_from = settings.EMAIL_HOST_USER
-    # sleep(20)
     send_mail(subject, message, email_from, emails)
